11c55930f5bdcf7408725daafc870042df391cba
AdDON <pdon.wus@gmail.com>
2025-06-12 14:59:30 +0200

    Rename file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-
 import './App.css';
+import Feedback from "./components/Feedback";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Layout from "./components/Layout";
+import Menu from "./components/Menu";
+import Offer from "./components/Offer";
+
 
 function App() {
   return (
