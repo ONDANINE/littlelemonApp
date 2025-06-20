e32d79f02a50be7fa997458e0852cf458805368a
AdDON <pdon.wus@gmail.com>
2025-06-20 13:52:24 +0200

    Update link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,28 @@
 import React from "react";
 import styles from "./Header.module.css";
 import littlelemonLogo from "../images/logolittlelemon.png";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useState } from "react";
 
 export default function Header() {
     return (
     <header className={styles.headerLayout}>
-        <nav>
-            <ul>
-                <li>About</li>
-                <li>Menus</li>
-                <li>Reservations</li>
-            </ul>
+        <nav className={styles.navbar}>
+            <Link
+                to="/about"
+            >
+              About
+            </Link>
+            <Link
+                to="/menus"
+            >
+              Menus
+            </Link>
+            <Link
+                to="/reservations"
+            >
+              Reservations
+            </Link>
         </nav>
         <img src={littlelemonLogo} alt="Restaurant Logo" />
         <button>Order Online</button>
