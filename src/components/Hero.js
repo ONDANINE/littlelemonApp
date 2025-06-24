7a0c8f308c7d35737ca41f47c0f03b06b3f8670d
AdDON <pdon.wus@gmail.com>
2025-06-24 16:45:28 +0200

    UPdate hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,9 +5,9 @@ export default function Hero() {
     return (
         <section className={styles.heroLayout}>
           <div>
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+              <h1>Little Lemon</h1>
+              <h2>Chicago</h2>
+              <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
           </div>
           <button>Looking for a table?</button>
         </section>
