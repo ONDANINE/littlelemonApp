886f504f15ab4524c097aa220957ed8b391f354a
AdDON <pdon.wus@gmail.com>
2025-06-30 16:44:42 +0200

    Update Image example

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -6,8 +6,10 @@ export default function Offer() {
         <main className={styles.offerLayout}>
           <button>Special Offers</button>
           <div className={styles.exclusiveMembers}>
-            <h2>Exclusive to members</h2>
-            <p>Description</p>
+            <div className = {styles.memberText}>
+              <h2>Exclusive to members</h2>
+              <p>Get points for dining in, takeout and even delivery. All our offers and promotions are offered exclusively to LITTLE LEMON members </p>
+            </div>
             <article>List of offerings</article>
             <button>Join Members</button>
           </div>
