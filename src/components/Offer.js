977b91b66197cb5cfe09f64669a73173c2fe7b0f
AdDON <pdon.wus@gmail.com>
2025-07-01 12:04:11 +0200

    Update Exclusive offer

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import offers from '../data/APIOffer';
 import styles from "./Offer.module.css";
 
 export default function Offer() {
@@ -10,7 +11,20 @@ export default function Offer() {
               <h2>Exclusive to members</h2>
               <p>Get points for dining in, takeout and even delivery. All our offers and promotions are offered exclusively to LITTLE LEMON members </p>
             </div>
-            <article>List of offerings</article>
+            <div>
+              {offers.map(offer => (
+                  <div key={offer.id} className={styles.offerCard}>
+                    <div className={styles.offerCardImageContainer}>
+                      <img src={offer.image} alt={offer.name} className={styles.offerCardImage} />
+                    </div>
+                    <div className={styles.offerCardText}>
+                      <h3>{offer.name}</h3>
+                      <p className={styles.offerCardDescription}>{offer.description}</p>
+                    </div>
+                  </div>
+                  ))
+                }
+            </div>
             <button>Join Members</button>
           </div>
         </main>
