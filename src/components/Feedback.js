0d224cdc32331e5ad8cab5016b1caaf79429b1f4
AdDON <pdon.wus@gmail.com>
2025-07-03 20:40:37 +0200

    update feedback and footer

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,18 +1,33 @@
 import React from "react";
 import styles from "./Feedback.module.css";
+import { IconName } from "react-icons/go";
+import { GoChevronLeft } from "react-icons/go";
+import { GoChevronRight } from "react-icons/go";
+
+
 
 
 export default function Feedback() {
     return (
-        <section className={styles.feedbackLayout}>
+        <div className={styles.feedbackLayout}>
           <h2>Feed back</h2>
-          <h3>Opinions</h3>
-          <p>We value your feedback!</p>
-          <button>Reviews</button>
-          <article>
-            <blockquote>"Best restaurant experience ever!" - Jane Doe</blockquote>
-            <blockquote>"The food was amazing and the service was top-notch!" - John Smith</blockquote>
-          </article>
-        </section>
+          <div className={styles.feedbackArea}>
+            <div className={styles.textArea}>
+              <h3>Opinions</h3>
+              <p>We’re Little Lemon - We love hearing from our guests and growing with your feedback.</p>
+            </div>
+            <button>Reviews</button>
+          </div>
+          <div className={styles.reviewCards}>
+          </div>
+          <div className={styles.buttonsGroup}>
+            <button className={styles.BTN}>
+              <GoChevronLeft className={styles.leftrightBTN}/>
+            </button>
+            <button className={styles.BTN}>
+              <GoChevronRight className={styles.leftrightBTN}/>
+            </button>
+          </div>
+        </div>
     )
 };
