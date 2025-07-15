c20727845bf75abcc81aefe058c3961ebd0763aa
AdDON <pdon.wus@gmail.com>
2025-07-15 19:59:54 +0200

    Update form

--- src/pages/Reservations.js
+++ src/pages/Reservations.js
@@ -1,13 +1,32 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import styles from "./Onprogress.module.css";
+import styles from "./Reservations.module.css";
 
 
 function Reservations() {
   return (
-    <div className={styles.onProgress}>
-      <h1>Reservations</h1>
-      <p>On progress!!!!</p>
+    <div className={styles.formContainer}>
+        <form className={styles.formContent}>
+            <label for="res-date">Choose date</label>
+            <input type="date" id="res-date"/>
+            <label for="res-time">Choose time</label>
+            <select id="res-time ">
+                <option>17:00</option>
+                <option>18:00</option>
+                <option>19:00</option>
+                <option>20:00</option>
+                <option>21:00</option>
+                <option>22:00</option>
+            </select>
+            <label for="guests">Number of guests</label>
+            <input type="number" placeholder="1" min="1" max="10" id="guests"/>
+            <label for="occasion">Occasion</label>
+            <select id="occasion">
+                <option>Birthday</option>
+                <option>Anniversary</option>
+            </select>
+            <input type="submit" value="Make Your reservation"/>
+        </form>
     </div>
   );
 }
